6260bad60b23b63d264d66a2d57a13ff404f17b1
Tim Miller <timcmiller@live.com>
2021-11-27 22:29:42 -0800

    Remove old unused files

--- app/rootReducer.ts
+++ app/rootReducer.ts
@@ -2,14 +2,11 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
 // eslint-disable-next-line import/no-cycle
-import counterReducer from './features/counter/counterSlice';
-// eslint-disable-next-line import/no-cycle
 import razzleReducer from './razzleReducer';
 
 export default function createRootReducer(history: History) {
   return combineReducers({
     router: connectRouter(history),
-    counter: counterReducer,
     razzle: razzleReducer,
   });
 }
